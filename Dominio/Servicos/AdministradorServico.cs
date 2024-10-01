376927b0fbd3ef1a0086d34afdbed05cf5d94462
Luis Cardoso <luiscard.queiroz@gmail.com>
2024-10-01 17:34:52 -0300

    criando servicos

---
+++ Dominio/Servicos/AdministradorServico.cs
@@ -0,0 +1,51 @@
+﻿using minimal_api.Dominio.DTO;
+using minimal_api.Dominio.Entidades;
+using minimal_api.Dominio.Interfaces;
+using minimal_api.Dominio.ModelViews;
+using minimal_api.Infraestrutura.Db;
+using System.Text.RegularExpressions;
+
+namespace minimal_api.Dominio.Servicos
+{
+    public class AdministradorServico : IAdministradorServico
+    {
+        private readonly BdContext _context;
+        public AdministradorServico(BdContext context)
+        {
+            _context = context;
+        }
+
+        public Administrador? BuscaPorId(int id)
+        {
+            return _context.Administradores.Find(id);
+        }
+
+        public Administrador Incluir(Administrador administrador)
+        {
+            _context.Add(administrador);
+            _context.SaveChanges();
+
+            return administrador;
+        }
+
+        public Administrador? Login(LoginDTO loginDTO)
+        {
+            Administrador? adm = _context.Administradores.FirstOrDefault(a => a.Email.Equals(loginDTO.Email) && a.Senha.Equals(loginDTO.Senha));
+
+            return adm;
+        }
+
+        public List<Administrador> Todos(int? pag)
+        {
+            int itensPorPagina = 10;
+            IQueryable<Administrador> query = _context.Administradores.AsQueryable();
+
+            if (pag.HasValue)
+            {
+                return query.Skip((pag.Value - 1) * itensPorPagina).Take(itensPorPagina).ToList();
+            }
+
+            return query.ToList();
+        }
+    }
+}
---
+++ Dominio/Servicos/VeiculoServico.cs
@@ -0,0 +1,53 @@
+﻿using minimal_api.Dominio.Entidades;
+using minimal_api.Dominio.Interfaces;
+using minimal_api.Infraestrutura.Db;
+using System.Linq;
+
+namespace minimal_api.Dominio.Servicos
+{
+    public class VeiculoServico : IVeiculoServico
+    {
+        private readonly BdContext _context;
+        public VeiculoServico(BdContext context)
+        {
+            _context = context;
+        }
+        public void Atualizar(Veiculo veiculo)
+        {
+            _context.Update(veiculo);
+            _context.SaveChanges();
+        }
+
+        public Veiculo? BuscaPorId(int id)
+        {
+            return _context.Veiculos.Find(id);
+        }
+
+        public void Excluir(Veiculo veiculo)
+        {
+            _context.Veiculos.Remove(veiculo);
+            _context.SaveChanges();
+        }
+
+        public void Incluir(Veiculo veiculo)
+        {
+            _context.Veiculos.Add(veiculo);
+            _context.SaveChanges(); 
+        }
+
+        public List<Veiculo> Todos(int? pag = 1, string? nome = null, string? marca = null)
+        {
+            int itensPorPagina = 10;
+            IQueryable<Veiculo> query = _context.Veiculos.AsQueryable();
+
+            if (!string.IsNullOrEmpty(nome)) query = query.Where(v => v.Nome.ToLower() == nome.ToLower());
+            if (!string.IsNullOrEmpty(marca)) query = query.Where(v => v.Marca.ToLower() == marca.ToLower());
+
+            if (pag.HasValue) {
+                return query.Skip((pag.Value - 1) * itensPorPagina).Take(itensPorPagina).ToList();
+            }
+            
+            return query.ToList();
+        }
+    }
+}
