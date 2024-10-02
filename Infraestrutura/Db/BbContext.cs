3c8cbd9a597ceb39ebfec36872294e8d9f671dcf
Luis Cardoso <luiscard.queiroz@gmail.com>
2024-10-01 21:35:07 -0300

    configurando db

--- Infraestrutura/Db/BbContext.cs
+++ Infraestrutura/Db/BbContext.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using minimal_api.Dominio.Entidades;
 
 namespace minimal_api.Infraestrutura.Db
 {
-    public class BbContext : DbContext
+    public class BdContext : DbContext
     {
-        
+        public BdContext(DbContextOptions<BdContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Administrador> Administradores { get; set; }
+        public DbSet<Veiculo> Veiculos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Administrador>().HasData(
+                new Administrador()
+                {
+                    AdministradorId = 1,
+                    Email = "administrador@teste.com",
+                    Senha = "123456",
+                    Perfil = "Adm"
+                }
+            );
+        }
     }
 }
